e22d11f73bd2ac9b259b839a15bd10b0fb8196e9
Nazlin <jemelinazlin@gmail.com>
2024-04-29 11:51:55 +0300

    completed

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -1,14 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 import TransactionsList from "./TransactionsList";
 import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
 function AccountContainer() {
+  const [transactions, setTransactions] = useState([])
+  const handleAddTransaction = (transaction) => {
+    console.log(transaction)
+    setTransactions(transactions =>[...transactions, transaction ])
+  }
+const [, setSearchTerm] = useState("")
+const [filteredTransactions, setFilteredTransactions] = useState([]);
+  
+  const handleSearch = (term) =>{
+    setSearchTerm(term)
+    setFilteredTransactions(
+      transactions.filter((transaction) =>
+      transaction.description.toLowerCase().includes(term.toLowerCase())
+      )
+    )
+  }
+  
   return (
     <div>
-      <Search />
-      <AddTransactionForm />
-      <TransactionsList />
+      <Search onSearch={handleSearch}/>
+      <AddTransactionForm onAddTransaction={handleAddTransaction}/>
+      <TransactionsList  transactions={filteredTransactions}/>
     </div>
   );
 }
--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import AccountContainer from "./AccountContainer";
 
-function App() {
+function App() { 
   return (
     <div className="ui raised segment">
       <div className="ui segment violet inverted">
